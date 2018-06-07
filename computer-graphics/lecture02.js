92389535f0c8a2cf76acef5f2b7c091a404111e9
YanaSlavcheva <yana.slavcheva@gmail.com>
2018-06-07 08:14:45 +0100

    Added lecture 2 - draw line

---
+++ computer-graphics/lecture02.js
@@ -0,0 +1,49 @@
+function drawLineClicked() {
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    ctx.clearRect(0, 0, 200, 150);
+
+    const canvasScaleFactor = 10;
+    const canvasWidth = 20;
+    const canvasHeight = 15;
+
+
+    const x0 = parseInt(document.getElementById('x0').value);
+    const x1 = parseInt(document.getElementById('x1').value);
+    const y0 = parseInt(document.getElementById('y0').value);
+    const y1 = parseInt(document.getElementById('y1').value);
+
+    const y0Reversed = canvasHeight - y0;
+    const y1Reversed = canvasHeight - y1;
+
+    drawLine(x0, x1, y0, y1);
+
+    // drawPixel(ctx, 0, 0, canvasScaleFactor);
+    // ctx.fillRect(10, 10, 10, 10);
+    // ctx.moveTo(0, 0);
+    // ctx.lineTo(1, 1);
+    // ctx.stroke();
+
+
+    // the canvas is scaled -> 1 pixel is actually rectangle 10 x 10 pixels
+    function drawPixel(ctx, xCoordinate, yCoordinate, canvasScaleFactor) {
+        ctx.fillRect(xCoordinate * canvasScaleFactor, yCoordinate * canvasScaleFactor, canvasScaleFactor, canvasScaleFactor);
+    }
+
+    // works only for x > y
+    function drawLine(x0, x1, y0, y1) {
+        let deltaX = x1 - x0;
+        let deltaY = y1 - y0;
+        let error = 0;
+        let deltaError = Math.abs(deltaY / deltaX)    
+        y = y0
+        for (let x = x0; x < x1; x++) {
+            drawPixel(ctx, x, y,canvasScaleFactor);
+            error = error + deltaError;
+            if (error >= 0.5) {
+                y = y + 1;
+                error = error - 1;
+            }
+        }
+    }
+}
